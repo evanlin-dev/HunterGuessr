4033ca89097c366c9812ce4c49b808f7e4ea92d5
evanlin-dev <evanlin2912@gmail.com>
2024-09-05 15:06:09 -0400

    Fix Desktop folder to include React JS

---
+++ Desktop/webpack.rules.js
@@ -0,0 +1,43 @@
+module.exports = [
+  // Native node module loader
+  {
+    test: /native_modules[/\\].+\.node$/,
+    use: 'node-loader',
+  },
+  {
+    test: /[/\\]node_modules[/\\].+\.(m?js|node)$/,
+    parser: { amd: false },
+    use: {
+      loader: '@vercel/webpack-asset-relocator-loader',
+      options: {
+        outputAssetBase: 'native_modules',
+      },
+    },
+  },
+  
+  // Babel loader for React/JSX files
+  {
+    test: /\.jsx?$/,
+    exclude: /node_modules/,
+    use: {
+      loader: 'babel-loader',
+      options: {
+        presets: ['@babel/preset-react'], // For React JSX support
+      }
+    }
+  },
+
+  // Typescript Example (if needed)
+  /**
+   * {
+   *   test: /\.tsx?$/,
+   *   exclude: /(node_modules|.webpack)/,
+   *   loaders: [{
+   *     loader: 'ts-loader',
+   *     options: {
+   *       transpileOnly: true
+   *     }
+   *   }]
+   * }
+   */
+];
