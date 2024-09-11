99ae5fda74618daaff05f395d197eee07fc86cfc
evanlin-dev <evanlin2912@gmail.com>
2024-09-11 18:37:20 -0400

    Working panoramic

--- Desktop/src/renderer/src/App.jsx
+++ Desktop/src/renderer/src/App.jsx
@@ -1,35 +1,74 @@
-import Versions from './components/Versions'
-import electronLogo from './assets/electron.svg'
+import React from "react";
+import { MarkersPlugin } from "@photo-sphere-viewer/markers-plugin";
+import { ReactPhotoSphereViewer } from "react-photo-sphere-viewer";
+import { LensflarePlugin } from "photo-sphere-viewer-lensflare-plugin";
+import "@photo-sphere-viewer/markers-plugin/index.css";
+import pinRed from './assets/pin-red.png';
+import panoImage from './assets/sciencecenter.png';
 
 function App() {
-  const ipcHandle = () => window.electron.ipcRenderer.send('ping')
+  const photoSphereRef = React.useRef();
+
+  const handleReady = (instance) => {
+    const markersPlugs = instance.getPlugin(MarkersPlugin);
+    if (!markersPlugs) return;
+    console.log(markersPlugs);
+    markersPlugs.addEventListener("select-marker", () => {
+      console.log("asd");
+    });
+  };
+
+  const plugins = [
+    [
+      MarkersPlugin,
+      {
+        // list of markers
+        markers: [
+          {
+            // image marker that opens the panel when clicked
+            id: "image",
+            position: { yaw: "95deg", pitch: "16deg" },
+            image: {pinRed},
+            anchor: "bottom center",
+            size: { width: 32, height: 32 },
+            tooltip: "Monte Civetta, Dolomites, Italy",
+          },
+        ],
+      },
+    ],
+    [
+      LensflarePlugin,
+      {
+        // list of lensflares
+        lensflares: [
+          {
+            id: "sun",
+            position: { yaw: "145deg", pitch: "2deg" },
+            type: 0,
+          },
+        ],
+      },
+    ],
+  ];
 
   return (
-    <>
-      <img alt="logo" className="logo" src={electronLogo} />
-      <div className="creator">Powered by electron-vite</div>
-      <div className="text">
-        Build an Electron app with <span className="react">React</span>
-      </div>
-      <p className="tip">
-        Please try pressing <code>F12</code> to open the devTool
-      </p>
-      <div className="actions">
-        <div className="action">
-          <a href="https://electron-vite.org/" target="_blank" rel="noreferrer">
-            Documentation
-          </a>
-        </div>
-        <div className="action">
-          <a target="_blank" rel="noreferrer" onClick={ipcHandle}>
-            Send IPC
-          </a>
-        </div>
-      </div>
-      <Versions></Versions>
-    </>
-  )
+    <div className="App">
+      <ReactPhotoSphereViewer
+        ref={photoSphereRef}
+        src={panoImage}
+        // defaultZoomLvl={0}
+        littlePlanet={true}
+        lang={{
+          littlePlanetButton: "Little Planet",
+        }}
+        hideNavbarButton={true}
+        height={"100vh"}
+        width={"100vw"}
+        onReady={handleReady}
+        plugins={plugins}
+      />
+    </div>
+  );
 }
 
-export default App
-
+export default App;
