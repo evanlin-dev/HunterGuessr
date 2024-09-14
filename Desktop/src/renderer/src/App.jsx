8a429a2d6976bfe33ed698f496f778d365234b84
evanlin-dev <evanlin2912@gmail.com>
2024-09-14 15:24:59 -0400

    Changed 360 library

--- Desktop/src/renderer/src/App.jsx
+++ Desktop/src/renderer/src/App.jsx
@@ -1,60 +1,36 @@
 import React from "react";
-import { MarkersPlugin } from "@photo-sphere-viewer/markers-plugin";
-import { ReactPhotoSphereViewer } from "react-photo-sphere-viewer";
-import "@photo-sphere-viewer/markers-plugin/index.css";
-import pinRed from './assets/pin-red.png';
-import panoImage from './assets/Test_Pano.jpg';
+import ReactPannellum, { getConfig } from "react-pannellum";
+import panoImage from './assets/sciencecenter.png';
 
-function App() {
-  const photoSphereRef = React.useRef();
+class App extends React.Component {
+  click() {
+    console.log(getConfig());
+  }
 
-  const handleReady = (instance) => {
-    const markersPlugs = instance.getPlugin(MarkersPlugin);
-    if (!markersPlugs) return;
-    console.log(markersPlugs);
-    markersPlugs.addEventListener("select-marker", () => {
-      console.log("asd");
-    });
-  };
+  render() {
+    const config = {
+      // autoRotate: -2,
+    };
 
-  const plugins = [
-    [
-      MarkersPlugin,
-      {
-        // list of markers
-        markers: [
-          {
-            // image marker that opens the panel when clicked
-            id: "image",
-            position: { yaw: "95deg", pitch: "16deg" },
-            image: {pinRed},
-            anchor: "bottom center",
-            size: { width: 32, height: 32 },
-            tooltip: "Monte Civetta, Dolomites, Italy",
-          },
-        ],
-      },
-    ]
-  ];
+    const equirectangularOptions = {
+      haov: 360,
+      vaov: 90, 
+    }
 
-  return (
-    <div className="App">
-      <ReactPhotoSphereViewer
-        ref={photoSphereRef}
-        src={panoImage}
-        defaultZoomLvl={0}
-        // littlePlanet={true}
-        lang={{
-          littlePlanetButton: "Little Planet",
-        }}
-        hideNavbarButton={true}
-        height={"100vh"}
-        width={"100vw"}
-        onReady={handleReady}
-        plugins={plugins}
-      />
-    </div>
-  );
+    return (
+      <div>
+        <ReactPannellum
+          id="1"
+          sceneId="firstScene"
+          style={{ width: "100vw", height: "100vh" }}
+          imageSource={panoImage}
+          config={config}
+          equirectangularOptions={equirectangularOptions}
+        />
+        <div onClick={this.click}>Click me</div>
+      </div>
+    );
+  }
 }
 
 export default App;
