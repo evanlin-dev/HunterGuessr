c58a1d989f39777cd2cbf6906e9c2e26afc1bee2
evanlin-dev <evanlin2912@gmail.com>
2024-09-22 20:01:14 -0400

    Add scene selector

--- Desktop/src/renderer/src/App.jsx
+++ Desktop/src/renderer/src/App.jsx
@@ -1,34 +1,10 @@
 import React from "react";
-import ReactPannellum, { getConfig } from "react-pannellum";
-import panoImage from './assets/sciencecenter.png';
+import Pano from "./pages/Pano";
 
 class App extends React.Component {
-  click() {
-    console.log(getConfig());
-  }
-
   render() {
-    const config = {
-      // autoRotate: -2,
-    };
-
-    const equirectangularOptions = {
-      haov: 360,
-      vaov: 90, 
-    }
-
     return (
-      <div>
-        <ReactPannellum
-          id="1"
-          sceneId="firstScene"
-          style={{ width: "100vw", height: "100vh" }}
-          imageSource={panoImage}
-          config={config}
-          equirectangularOptions={equirectangularOptions}
-        />
-        <div onClick={this.click}>Click me</div>
-      </div>
+      <Pano />
     );
   }
 }
