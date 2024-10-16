bbde8d5bb5a3b0701a95d9e7990ed12dc81f322e
evanlin-dev <evanlin2912@gmail.com>
2024-10-16 13:03:45 -0400

    vercel

--- Website/vite.config.js
+++ Website/vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_BASE_PATH || '/HunterGuessr/',
+  base: process.env.VITE_BASE_PATH || '/',
 })
