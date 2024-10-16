3b42ec5227b710f743272f1184563827c5c6331d
evanlin-dev <evanlin2912@gmail.com>
2024-10-16 12:29:56 -0400

    test

--- Website/vite.config.js
+++ Website/vite.config.js
@@ -3,10 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/HunterGuessr/',
-  build: {
-    rollupOptions: {
-      external: ['axios'],
-    },
-  }
+  base: '/',
 })
